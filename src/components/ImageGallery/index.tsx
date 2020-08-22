d5987315903d7fa5016a9163de9313a0a44e42a6
matiNap <mateusz.napieralski04@gmail.com>
2020-08-22 14:03:02 +0200

    fix: Fix image gallery

--- src/components/ImageGallery/index.tsx
+++ src/components/ImageGallery/index.tsx
@@ -1,5 +1,21 @@
 import React from 'react';
-import Animated, { useCode, cond, eq, set, add } from 'react-native-reanimated';
+import Animated, {
+    useCode,
+    cond,
+    eq,
+    set,
+    add,
+    call,
+    divide,
+    abs,
+    clockRunning,
+    max,
+    and,
+    greaterOrEq,
+    lessOrEq,
+    min,
+    diff,
+} from 'react-native-reanimated';
 import Props from '../../types/ImageGallery';
 import { Image, View, StyleSheet } from 'react-native';
 import metrics from '../../theme/metrics';
@@ -9,23 +25,40 @@ import {
     snapPoint,
     useValue,
     timing,
+    useClock,
 } from 'react-native-redash';
 
-export default ({ images, imageHeight, containerStyle }: Props) => {
+export default ({
+    images,
+    imageHeight,
+    containerStyle,
+    children,
+    onChange,
+}: Props) => {
+    const clock = useClock();
     const offsetX = useValue(0);
     const translateX = useValue(0);
     const width = metrics.window.width;
     const snapPoints = images.map((_, index) => index * -width);
-    const imageContainerWidth = metrics.window.width * images.length;
+    const imagesContainerWidth = metrics.window.width * images.length;
     const {
         gestureHandler,
         state,
         translation,
         velocity,
     } = usePanGestureHandler();
-    const to = snapPoint(translateX, velocity.x, snapPoints);
+    const to = snapPoint(
+        translateX,
+        cond(
+            and(greaterOrEq(velocity.x, -2000), lessOrEq(velocity.x, 2000)),
+            velocity.x,
+            min(-1, max(1, velocity.x)),
+        ),
+        snapPoints,
+    );
     useCode(
         () => [
+            // debug('Test', diff(clockRunning(clock))),
             cond(eq(state, State.ACTIVE), [
                 set(translateX, add(offsetX, translation.x)),
             ]),
@@ -35,7 +68,21 @@ export default ({ images, imageHeight, containerStyle }: Props) => {
                     timing({
                         from: translateX,
                         to,
+                        clock,
                     }),
+                ),
+                cond(
+                    eq(diff(clockRunning(clock)), -1),
+                    call(
+                        [
+                            abs(divide(translateX, metrics.window.width)),
+                            to,
+                            translateX,
+                        ],
+                        ([current]) => {
+                            onChange(current + 1);
+                        },
+                    ),
                 ),
                 set(offsetX, translateX),
             ]),
@@ -47,24 +94,40 @@ export default ({ images, imageHeight, containerStyle }: Props) => {
             <PanGestureHandler {...gestureHandler}>
                 <Animated.View
                     style={StyleSheet.flatten([
-                        styles.imageContainer,
+                        styles.imagesContainer,
                         {
                             transform: [{ translateX }],
-                            width: imageContainerWidth,
+                            width: imagesContainerWidth,
                         },
                     ])}
                 >
                     {images.map((image) => (
-                        <Image
-                            source={{ uri: image }}
-                            style={StyleSheet.flatten([
-                                styles.image,
-                                { height: imageHeight },
-                            ])}
-                        />
+                        <View style={styles.imageContainer} key={image.key}>
+                            <Image
+                                source={{ uri: image.uri }}
+                                style={StyleSheet.flatten([
+                                    styles.image,
+                                    { height: imageHeight },
+                                ])}
+                            />
+                            <View
+                                style={[
+                                    styles.absoluteContainer,
+                                    { height: imageHeight },
+                                ]}
+                            >
+                                {image.children}
+                            </View>
+                        </View>
                     ))}
                 </Animated.View>
             </PanGestureHandler>
+            <View
+                style={[styles.absoluteContainer, { height: imageHeight }]}
+                pointerEvents="box-none"
+            >
+                {children}
+            </View>
         </View>
     );
 };
@@ -74,7 +137,17 @@ const styles = StyleSheet.create({
         width: metrics.window.width,
         height: '100%',
     },
-    imageContainer: {
+    imagesContainer: {
         flexDirection: 'row',
+    },
+    imageContainer: {
+        width: metrics.window.width,
+        height: '100%',
+    },
+    absoluteContainer: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: metrics.window.width,
     },
 });
